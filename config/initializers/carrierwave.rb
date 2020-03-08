c6098e8a8a782ad8354cd1a1e996f0cfc2fb927f
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-03-08 21:03:47 +0900

    デプロイ修正２

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  # if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
-  #   config.storage = :file
-  # elsif Rails.env.production? #本番はS3に保存する
+  if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -14,8 +12,12 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 
     config.fog_directory  = '64dimage'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/64dimage'
-  # end
+  
 end
