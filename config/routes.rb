c4dbcb68dc7f0a3e4eba50c28e555c3de4f79c7e
yuyahhh <yuyasakai6120@gmail.com>
2020-03-11 00:50:50 +0900

    修正途中と商品詳細追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  # root to: 'products#show'
+  # root to: 'buys#index'
+  resources :products, only: [:show] do
+    resources :buys, only: [:show]
+  end
   
 end
