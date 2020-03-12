823fe46b16868b19822762ccddfeffe4fc273743
yuyahhh <yuyasakai6120@gmail.com>
2020-03-12 20:18:04 +0900

    修正完了

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # root to: 'products#show'
   # root to: 'buys#index'
   resources :products, only: [:show] do
-    resources :buys, only: [:show]
+    resources :buys, only: [:index]
   end
   
 end
