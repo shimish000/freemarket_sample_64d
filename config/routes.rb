c3c2f6daa36c3cc9d85a952a609826f284f6c699
yuyahhh <yuyasakai6120@gmail.com>
2020-03-14 17:10:13 +0900

    ルートの修正とリンクのpath指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-  # root to: 'sells#new'
-  # root to: 'products#show'
-  # root to: 'buys#index'
+
   resources :products, only: [:show] do
     resources :buys, only: [:index]
+    resources :sells, only: [:new, :create]
   end
   
 end
