0035315af22c934384a453ce64dc868892c22673
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-03-14 17:38:59 +0900

    マイページのパス繋げ

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
 
-  root to: 'sells#new'
+  root to: 'users#index'
 
   # root to: 'products#show'
   # root to: 'buys#index'
   resources :products, only: [:show] do
     resources :buys, only: [:index]
   end
-  resources :users, only: [:index, :destroy]
+  resources :users, only: [:index, :destroy] 
   resources :cards, only: [:create, :new]
   
 end
