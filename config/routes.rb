9c1ef35c57ae7f387401cc186e1e2e77b7a8e3b1
yuyahhh <yuyasakai6120@gmail.com>
2020-03-14 16:54:54 +0900

    トップページとルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: 'sells#new'
+  root to: 'products#index'
+  # root to: 'sells#new'
   # root to: 'products#show'
   # root to: 'buys#index'
   resources :products, only: [:show] do
