586224792ab8284f2f8806109b41ebc201d263f0
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-03-14 16:24:03 +0900

    cardsテーブルの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'cards#new'
 
-  root to: 'products#index'
+  root to: 'cards#new'
   # root to: 'products#show'
   # root to: 'buys#index'
   resources :products, only: [:show] do
