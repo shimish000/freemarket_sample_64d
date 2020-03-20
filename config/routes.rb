a2f84a3dbf3965108b0d4c789fa463f2607efba0
xignature2 <58798583+xignature2@users.noreply.github.com>
2020-03-20 20:13:29 +0900

    実装前

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:show] do
     resources :buys, only: [:index]
-    resources :sells, only: [:new, :create]
+    resources :sells, only: [:index, :new, :create, :edit, :update, :destroy]
   end
   resources :users, only: [:index, :destroy] 
   resources :cards, only: [:create, :new]
