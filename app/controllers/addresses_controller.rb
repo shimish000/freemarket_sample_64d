f14914f75c65bba4d2152e97fe07dd0947bee990
yuyahhh <yuyasakai6120@gmail.com>
2020-04-02 01:51:11 +0900

    アドレス登録機能完了

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -10,6 +10,6 @@ class AddressesController < ApplicationController
   
   private
   def address_params
-    params.require(:address).permit(:first_name, :last_name).merge(user_id:)
+    params.require(:address).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :number, :prefecture_id, :municipality, :address, :building_name, :address_phone).merge(user_id: current_user.id)
   end
 end
