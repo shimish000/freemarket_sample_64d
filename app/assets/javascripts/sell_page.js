de598e41c8d7b86713cdd760a54cfe6f384abada
xignature2 <58798583+xignature2@users.noreply.github.com>
2020-04-02 20:36:33 +0900

    実装途中

--- app/assets/javascripts/sell_page.js
+++ app/assets/javascripts/sell_page.js
@@ -78,3 +78,59 @@ $(document).on('turbolinks:load', ()=> {
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 });
+
+
+$(function(){
+  //DataTransferオブジェクトで、データを格納する箱を作る
+  var dataBox = new DataTransfer();
+  //querySelectorでfile_fieldを取得
+  var file_field = document.querySelector('input[type=file]')
+  //fileが選択された時に発火するイベント
+  $('#aaa').change(function(){
+    //選択したfileのオブジェクトをpropで取得
+    var files = $('input[type="file"]').prop('files')[0];
+    $.each(this.files, function(i, file){
+      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      var fileReader = new FileReader();
+
+      //DataTransferオブジェクトに対して、fileを追加
+      dataBox.items.add(file)
+      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
+      file_field.files = dataBox.files
+
+      var num = $('.item-image').length + 1 + i
+      fileReader.readAsDataURL(file);
+       //画像が10枚になったら超えたらドロップボックスを削除する
+      if (num == 10){
+        $('#image-box__container').css('display', 'none')   
+      }
+      //読み込みが完了すると、srcにfileのURLを格納
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        var html= `<div class='item-image' data-image="${file.name}">
+                    <div class=' item-image__content'>
+                      <div class='item-image__content--icon'>
+                        <img src=${src} width="114" height="80" >
+                      </div>
+                    </div>
+                    <div class='item-image__operetion'>
+                      <div class='item-image__operetion--delete'>削除</div>
+                    </div>
+                  </div>`
+        //image_box__container要素の前にhtmlを差し込む
+        $('#image-box__container').before(html);
+      };
+      //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
+      $('#image-box__container').attr('class', `item-num-${num}`)
+    });
+  });
+  //削除ボタンをクリックすると発火するイベント
+  $(document).on("click", '.item-image__operetion--delete', function(){
+    //プレビュー要素を取得
+    var target_image = $(this).parent().parent()
+    //プレビューを削除
+    target_image.remove();
+    //inputタグに入ったファイルを削除
+    file_field.val("")
+  })
+});
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,4 @@
 class Image < ApplicationRecord
   mount_uploader :src, ImageUploader
   belongs_to :product
-#   mount_uploader :image_url, ImageUploader
 end
