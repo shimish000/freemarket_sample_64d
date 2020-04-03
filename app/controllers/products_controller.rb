f147041f942bae5e39989c0442a27d2c769da7ec
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-04-03 15:44:39 +0900

    categoryの途中

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, except: [:index, :new, :create]
+  before_action :set_product, except: [:index, :new, :create, :get_category_children]
 
   def index
     @products = Product.includes(:images).order('created_at DESC')
@@ -10,19 +10,18 @@ class ProductsController < ApplicationController
 
   end
 
-  def new
-    @children = Category.find(params[:parent_id]).children
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
-
 
   def new
     @product = Product.new
     @product.images.new
-  end  
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+       @category_parent_array << parent.name
+    end
+ end
+  
+  
+    
 
   def create
     @product = Product.new(product_params)
@@ -48,6 +47,14 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     redirect_to root_path
+  end
+
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 
   private
