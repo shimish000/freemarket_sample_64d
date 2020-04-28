6022c91adb4f8146ed3d63bf8b5dd1a90b6657ef
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-04-29 07:28:21 +0900

    likeの例外処理

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,24 @@
 class LikesController < ApplicationController
-  before_action :get_product, only: [:create,:destroy]
-
   def create
-    @like = Like.create(user_id: current_user.id, product_id: params[:product_id])
+    @like = Like.new(user_id: current_user.id, product_id: params[:product_id])
+    if @like.save
+    else
+      flash.now[:alert] = @like.errors.full_messages
+    end
     @likes = Like.where(product_id: params[:product_id])
+    get_product
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, product_id: params[:product_id])
-    @like.destroy
+    if @like.destroy
+    else
+      flash.now[:alert] = '削除できませんでした。'
+    end
     @likes = Like.where(product_id: params[:product_id])
+    get_product
   end
 
-  private
   def get_product
     @product = Product.find(params[:product_id])
   end
