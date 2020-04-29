a28dfb7b93a23288e2e9626afd9204af82ee6a0f
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-04-29 15:36:51 +0900

    schemaの訂正２

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# ActiveRecord::Schema.define(version: 2020_04_24_053414) do
-ActiveRecord::Schema.define(version: 2020_04_22_084255) do
+ActiveRecord::Schema.define(version: 2020_04_24_053414) do
+# ActiveRecord::Schema.define(version: 2020_04_22_084255) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
